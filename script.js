b9337b92b619c207646f36b1d410a758c5aa8a40
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2026-03-03 06:39:58 -0500

    Refactor chess piece variables and preload images

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-var frog;
+var B_bishop; var B_rook; var B_knight; var B_king; var B_queen; var B_pawn;
+var W_bishop; var W_rook; var W_knight; var W_king; var W_queen; var W_pawn;
 var sqaureSide=120;
 
 function preload(){
-  frog=loadImage('B_bishop.png');
+  B_bishop=loadImage('B_bishop.png');
+  B_rook=loadImage('B_rook.png');
+  B_knight=loadImage('B_knight.png');
+  B_king=loadImage('B_king.png');
+  B_queen=loadImage('B_queen.png');
+  B_pawn=loadImage('B_pawn.png');
+  W_bishop=loadImage('W_bishop.png');
+  W_rook=loadImage('W_rook.png');
+  W_knight=loadImage('W_knight.png');
+  W_king=loadImage('W_king.png');
+  W_queen=loadImage('W_queen.png');
+  W_pawn=loadImage('W_pawn.png');
 }
 
 
@@ -17,7 +29,7 @@ function drawBoard(){
   stroke(0);
   strokeWeight(3);
   fill(255)
-  for(var g=0;g<5;g++){
+  for(var g=0;g<3;g++){
     rect(100+g*200,30,200,200);
   }
 }
