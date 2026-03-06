c945b340cf8b8e8bfce91fa3954060567543d9ce
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2026-03-06 09:55:05 -0500

    Add preload function for loading chess piece images

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var sqaureSide=120;
 var whitesTurn=true;
 var whiteFreqs=[0,0,0,0,0,0];  //for history, both colors orders will be
 var blackFreqs=[0,0,0,0,0,0];  // [pawns, rooks, knights, bishops, queens, kings]
+
 function preload(){
   B_bishop=loadImage('B_bishop.png');
   B_rook=loadImage('B_rook.png');
