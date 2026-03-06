042142bc74b945d06b56a657fffc4e5e04c1010c
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2026-03-06 10:27:44 -0500

    Adjust image positions in showFreqs function

--- script.js
+++ script.js
@@ -107,8 +107,8 @@ function pickBlack(){
 function showFreqs(){
   var boardIndexes=[0,8,10,12,14,15];
   for(t=0;t<boardIndexes.length;t++){
-    image(whitePieces[boardIndexes[t]],1000,20+36*t,36,36);
-    image(blackPieces[boardIndexes[t]],1000,240+36*t,36,36);
+    image(whitePieces[boardIndexes[t]],1000,30+36*t,36,36);
+    image(blackPieces[boardIndexes[t]],1000,300+36*t,36,36);
   }
 }
 
